e48500a54123b1052f68d599def935ddbc04783a
Enunwah Stephen <senunwah@yahoo.com>
2019-01-24 17:53:57 +0100

    Modified to accept requests

--- src/main/java/com/interswitch/zuul/Configuration/ApplicationConfiguration.java
+++ src/main/java/com/interswitch/zuul/Configuration/ApplicationConfiguration.java
@@ -6,18 +6,18 @@ import org.springframework.cloud.netflix.zuul.filters.post.LocationRewriteFilter
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableZuulProxy
+//@Configuration
+//@EnableZuulProxy
 public class ApplicationConfiguration {
 
-    @Bean
+    //@Bean
     public PatternServiceRouteMapper serviceRouteMapper() {
         return new PatternServiceRouteMapper(
                 "(?<name>^.+)-(?<version>v.+$)",
                 "${version}/${name}");
     }
 
-    @Bean
+    //@Bean
     public LocationRewriteFilter locationRewriteFilter(){
         return new LocationRewriteFilter();
     }
